9d5e3d279885203cc7c7255d591347d05a57558b
Mike <mike.sierra.7950@gmail.com>
2021-06-30 13:59:58 -0400

    Exercise started on new branch

--- CarTests/CarTests.cs
+++ CarTests/CarTests.cs
@@ -29,7 +29,19 @@ namespace CarTests
             Assert.AreEqual(4, test_car.NumberOfWheels);
         }
         //TODO: gasTankLevel is accurate after driving within tank range
+        [TestMethod]
+        public void TestGasTankAfterDriving()
+        {
+            test_car.Drive(50);
+            Assert.AreEqual(9, test_car.GasTankLevel, .001);
+        }
         //TODO: gasTankLevel is accurate after attempting to drive past tank range
+        [TestMethod]
+        public void TestGasTankAfterExceedingTankRange()
+        {
+            test_car.Drive(700);
+            Assert.IsTrue(0 == test_car.GasTankLevel);
+        }
         //TODO: can't have more gas than tank size, expect an exception
 
     }
